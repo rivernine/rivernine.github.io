04e765abf287326b64f8ac4fe0d624ef94447101
Jack <rkdworn9411@gmail.com>
2020-06-27 02:45:42 +0900

    Updates

---
+++ precache-manifest.59dfa80c06ac474db4d54d1819d9a68a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a84195d95c2d2594275d184db67c5fcf",
+    "url": "/rivernine.github.io/index.html"
+  },
+  {
+    "revision": "7bc899092431a30bd419",
+    "url": "/rivernine.github.io/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "cec621bdbb7aa17e992b",
+    "url": "/rivernine.github.io/static/js/2.d393c82c.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/rivernine.github.io/static/js/2.d393c82c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7bc899092431a30bd419",
+    "url": "/rivernine.github.io/static/js/main.fbe27d95.chunk.js"
+  },
+  {
+    "revision": "edbfef6dc3a4b4c0c099",
+    "url": "/rivernine.github.io/static/js/runtime-main.36b0537b.js"
+  },
+  {
+    "revision": "385019043696752f78fc6cf5e12a94bc",
+    "url": "/rivernine.github.io/static/media/icon.38501904.svg"
+  }
+]);
